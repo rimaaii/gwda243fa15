0d1b1506a6f2465d20c38e023c2446a8c7a8730d
rimaaii <rgerhard@aii.edu>
2015-10-07 10:45:12 -0400

    Create tinder.js

---
+++ class1/tinder.js
@@ -0,0 +1,32 @@
+//Tinder Objects
+
+//creating first object instances
+//containing properties - no methods yet!
+var user1 = {
+				name : "John Doe",
+				gender : "Male", 
+				email : "john@doe.com",
+				location : "Miami, FL", 
+				description : "Corporate Executive", 		
+				orientation : "Straight", 
+				age : 35,
+				picture : "profile.jpg",
+				status : 1,
+				facebookProfile : "facebook.com/johndoe",
+				interests : "Skiing, Surfing"
+			};
+			
+//Create additional user for the app	
+var user2 = {
+				name : "Jane Doe",
+				gender : "Female", 
+				email : "jane@doe.com",
+				location : "Miami Beach, FL", 
+				description : "Yoga Teacher", 		
+				orientation : "Straight", 
+				age : 38,
+				picture : "profile.jpg",
+				status : 1,
+				facebookProfile : "facebook.com/janedoe",
+				interests : "Yoga, Running"
+			};		
